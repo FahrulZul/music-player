d4be971008bb53223f9adf8437ae52eced89d707
fahrulzul <fahrulzul96@gmail.com>
2021-07-29 19:31:43 +0800

    modofied

--- js/data/songs.js
+++ js/data/songs.js
@@ -7,79 +7,65 @@ export const songsList = [{
   },
   {
     "id": "2",
-    "url": "../../music/crooked.mp3",
+    "url": "https://www.dropbox.com/s/wu0b6iu3nt6565a/crooked.mp3?dl=1",
     "time": "03:45",
     "title": "Crooked",
     "artist": "G-Dragon"
   },
   {
     "id": "3",
-    "url": "../../music/fall-for-you.mp3",
+    "url": "https://www.dropbox.com/s/4ye9506z39rnw54/fall-for-you.mp3?dl=1",
     "time": "03:05",
     "title": "Fall For You",
     "artist": "Secondhand Serenade"
   },
   {
     "id": "4",
-    "url": "../../music/giyomi.mp3",
+    "url": "https://www.dropbox.com/s/cf2pjzwrtxcbsxc/giyomi.mp3?dl=1",
     "time": "02:43",
     "title": "Gwiyomi",
     "artist": "Hari"
   },
   {
     "id": "5",
-    "url": "../../music/miraie.mp3",
+    "url": "https://www.dropbox.com/s/fqp0o6a7jifvv0g/miraie.mp3?dl=1",
     "time": "05:31",
     "title": "Mirai e",
     "artist": "Kiroro"
   },
   {
     "id": "6",
-    "url": "../../music/on-a-joyride.mp3",
+    "url": "https://www.dropbox.com/s/49w9dpu1acqejcc/on-a-joyride.mp3?dl=1",
     "time": "03:37",
     "title": "5 on a Joyride",
     "artist": "Cody Chesnutt"
   },
   {
     "id": "7",
-    "url": "../../music/shower.mp3",
+    "url": "https://www.dropbox.com/s/bwd5idliyge2yda/shower.mp3?dl=1",
     "time": "03:26",
     "title": "Shower",
     "artist": "Becky G"
   },
   {
     "id": "8",
-    "url": "../../music/some-nights.mp3",
+    "url": "https://www.dropbox.com/s/n4p9gktcb5c4njj/some-nights.mp3?dl=1",
     "time": "04:37",
     "title": "Some Nights",
     "artist": "Fun"
   },
   {
     "id": "9",
-    "url": "../../music/the-day-you-went-away.mp3",
+    "url": "https://www.dropbox.com/s/ernwjp5j540z5qs/the-day-you-went-away.mp3?dl=1",
     "time": "03:41",
     "title": "The Day You Went Away",
     "artist": "M2M"
   },
   {
     "id": "10",
-    "url": "../../music/youre-gonna-miss-me.mp3",
+    "url": "https://www.dropbox.com/s/3g8gcaefkybhyyg/youre-gonna-miss-me.mp3?dl=1",
     "time": "02:06",
     "title": "You're Gonna Miss Me",
     "artist": "Anna Kendrick"
-  },
-  // {
-  //   "id": "11",
-  //   "url": "../../music/rockstar.mp3",
-  //   "time": "04:01",
-  //   "title": "Rockstar",
-  //   "artist": "Post Malone"
-  // },
-  // {
-  //   "id": "12",
-  //   "url": "../../music/sad.mp3",
-  //   "time": "02:46",
-  //   "title": "Sad",
-  //   "artist": "XXXtentacion"
-  // },
+  }
 ];
